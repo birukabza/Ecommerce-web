d4eaba622d7a376e9dec1321310e2b9cdaec86e5
Biruk Abza <Birukabza@gmail.com>
2024-11-06 20:52:14 +0300

    commented out code in server.js file that serves the frontend

--- server/server.js
+++ server/server.js
@@ -58,13 +58,13 @@ app.post("/create-payment-intent", async (req, res) => {
 })
 
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
-    })
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
+//     })
+// }
 
 
 app.listen(port, (error) => {
