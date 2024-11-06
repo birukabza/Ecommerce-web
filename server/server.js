02fee62e642278642beba9c6b30f23320546cd56
Biruk Abza <Birukabza@gmail.com>
2024-11-06 20:17:18 +0300

    made changes to the __dirname in server.js because of errors caused while deploying (Error: __dirname is not an ES module)

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ import path from "path";
 import helmet from "helmet";
 import Stripe from "stripe";
 import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 dotenv.config()
 
